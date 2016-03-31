ef1297423cd1a49dacbe949009d1090cb8c1f832
Nader Dabit <dabit3@gmail.com>
2016-03-31 17:07:46 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './src',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './src',
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
