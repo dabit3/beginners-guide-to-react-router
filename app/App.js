2ebc7a6e173e213a6697aaee9127a3cef7170f1b
Nader Dabit <dabit3@gmail.com>
2016-04-04 15:55:59 -0500

    Added browserHistory

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
-const App = () => <h1>Hello World!!!!!!</h1>
+const App = () => <h1>Hello World!</h1>
 
 export default App
